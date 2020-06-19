fe5fb552d7d72278aab3c888f980c1f05e634a6b
Piotr <piotr.guziak.mail@gmail.com>
2020-06-19 16:09:41 +0200

    Created first really basic platform to expand from
    Couple buttons that open and close windows. It doesn't have any sense right now.

---
+++ TextHighlightApp/BasicMechanism/AreYouSure.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BasicMechanism
+{
+    /// <summary>
+    /// Interaction logic for AreYouSure.xaml
+    /// </summary>
+    public partial class AreYouSure : Window
+    {
+        public AreYouSure()
+        {
+            InitializeComponent();
+        }
+
+        private void No_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Yes_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ TextHighlightApp/BasicMechanism/RuleAddWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BasicMechanism
+{
+    /// <summary>
+    /// Interaction logic for RuleAddWindow.xaml
+    /// </summary>
+    public partial class RuleAddWindow : Window
+    {
+        public RuleAddWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAccept_Click(object sender, RoutedEventArgs e)
+        {
+            string text = RuleText.Text;
+            //Application.Current.MainWindow
+            this.Close();
+        }
+
+        private void ButtonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            AreYouSure askingWindow = new AreYouSure();
+            askingWindow.ShowDialog();
+        }
+    }
+}
